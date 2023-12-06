59dd304f018589f4d7b5302725a06ac8f5af7400
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-06 18:05:53 +0100

    Update Board.py

--- Pyxel Learning/Board.py
+++ Pyxel Learning/Board.py
@@ -3,24 +3,7 @@ from Mario import Mario
 import pyxel
 from Block import Block
 
-WIDTH = 256
-HEIGHT = 224
-FLOOR = 100
 
-# Mario
-MARIO_INITIAL = (WIDTH / 2, 200)
-MARIO_SPRITE = (1, 0, 0, 16, 16)
-
-
-# Enemies
-SPRITE_BLOCK_1 = (0, 0, 0, 16, 16)
-SPRITE_BLOCK_2 = (0, 32, 0, 16, 16)
-SPRITE_POW = (0, 16, 0, 16, 16)
-BLOCK_INITIAL = ((0, 40, "BLOCK1"), (16, 40, "BLOCK1"),
-                 (32, 40, "BLOCK1"), (48, 40, "BLOCK1"),
-                 (124, 150, "POW"), (240, 40, "BLOCK2"),
-                 (224, 40, "BLOCK2"), (208, 40, "BLOCK2"),
-                 (192, 40, "BLOCK2"))
 
 
 
