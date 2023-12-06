1f4e0ac5ccf4ee5efb68ece64bf80dd6be90a650
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-06 19:01:44 +0100

    Update Block.py

--- Pyxel Learning/Block.py
+++ Pyxel Learning/Block.py
@@ -1,9 +1,11 @@
 import pyxel
-
 import Constants
 
-class Block:
 
+class Block:
+    '''This is a Class For the blocks to be printed
+    it runs through the Constants file where the dimmensions are stored and then prints a block
+    in each desired area'''
     def __init__(self, x: int, y: int, block_type: str):
         ''' Init method, creates the initial attributes of the block '''
         self.x = x
@@ -16,16 +18,8 @@ class Block:
 
 
     def draw(self):
-        if self.block_type == "BLOCK1":
-            pyxel.blt(self.x, self.y, 0, 32, 72, self.height, self.width, colkey=0)
-        elif self.block_type == "BLOCK2":
-            pyxel.blt(self.x, self.y, 0, 0, 64, self.height, self.width, colkey=0)
-        elif self.block_type == "POW":
-            pyxel.blt(self.x, self.y, 0, 0, 64, self.height, self.width, colkey=0)
-        else:
-            print("Block called "+self.block_type + " doesn't exist")
 
-    """ These are the methods that will be used to assign values to the 
-    attributes """
+        pyxel.blt(self.x, self.y, 0, 32, 72, self.height, self.width, colkey=0)
+
 
 
