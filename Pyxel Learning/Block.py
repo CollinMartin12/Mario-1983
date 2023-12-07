0bf5de19f14c2b9e7457af9338bca7744fae17c6
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-07 14:05:58 +0100

    Update Block.py

--- Pyxel Learning/Block.py
+++ Pyxel Learning/Block.py
@@ -6,20 +6,29 @@ class Block:
     '''This is a Class For the blocks to be printed
     it runs through the Constants file where the dimmensions are stored and then prints a block
     in each desired area'''
+
     def __init__(self, x: int, y: int, block_type: str):
         ''' Init method, creates the initial attributes of the block '''
         self.x = x
         self.y = y
         self.block_type = block_type
-        self.width = 8
-        self.height = 32
+        self.width = 32
+        self.height = 8
 
-
-
-
-    def draw(self):
-
-        pyxel.blt(self.x, self.y, 0, 32, 72, self.height, self.width, colkey=0)
+    @property
+    def sprite(self) -> tuple:
+        global sprite
+        if self.block_type == "BLOCK1":
+            sprite = Constants.SPRITE_BLOCK_1
+        elif self.block_type == "BLOCK2":
+            sprite = Constants.SPRITE_BLOCK_2
+        elif self.block_type == "POW":
+            sprite = Constants.SPRITE_POW
+        elif self.block_type == "PIPE_LEFT":
+            sprite = Constants.SPRITE_PIPE_LEFT
+        elif self.block_type == "PIPE_RIGHT":
+            sprite = Constants.SPRITE_PIPE_RIGHT
+        return sprite
 
 
 
