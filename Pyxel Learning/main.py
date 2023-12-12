3a7987ad7f3ac99575950b529d71877622e817ed
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-12 22:21:47 +0100

    Update main.py

--- Pyxel Learning/main.py
+++ Pyxel Learning/main.py
@@ -3,22 +3,16 @@ import Constants
 from Board import Board
 from Player import Player
 
+
 board = Board()
 
 # Initializes pyxel
 pyxel.init(board.width, board.height, "Mario")  # type: ignore
 # Loads the resource file
-pyxel.load("my_resource.pyxres")
+pyxel.load("compact.pyxres")
+
+
 # Runs the game
 pyxel.run(board.update, board.draw)
-
-
-    # game = Mario()
-    board = Board(300, 300)
-    pyxel.load("my_resource.pyxres")  # Load your .pyxel image file
-    pyxel.run(board.update, board.draw)
-
-if __name__ == "__main__":
-    run_game()
 
 
