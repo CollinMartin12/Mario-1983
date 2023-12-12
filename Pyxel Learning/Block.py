5dc083837898dcd0bb793eb567570460b4a1161c
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-12 22:17:50 +0100

    Update Block.py

--- Pyxel Learning/Block.py
+++ Pyxel Learning/Block.py
@@ -8,18 +8,37 @@ class Block:
     in each desired area'''
 
     def __init__(self, x: int, y: int, block_type: str):
-        ''' Init method, creates the initial attributes of the block '''
+        """ Init method, creates the initial attributes of the block """
         self.x = x
         self.y = y
         self.block_type = block_type
-        self.width = 32
+        self.width = 16
         self.height = 8
+        self.block_hit = False
+        self.animation_frames = 0
+
+
+    def update(self):
+        if self.block_hit:
+            if self.animation_frames < 3:
+
+                self.block_type = f'HIT{self.animation_frames + 1}'
+                print(self.block_type)
+
+                self.animation_frames += 1
+            else:
+
+                self.block_hit = False
+                self.block_type = 'BLOCK1'
+                self.animation_frames = 0
 
     @property
     def sprite(self) -> tuple:
         global sprite
         if self.block_type == "BLOCK1":
             sprite = Constants.SPRITE_BLOCK_1
+        elif self.block_type == 'HIT1':
+            sprite = Constants.HIT_BLOCK_1
         elif self.block_type == "BLOCK2":
             sprite = Constants.SPRITE_BLOCK_2
         elif self.block_type == "POW":
@@ -28,7 +47,6 @@ class Block:
             sprite = Constants.SPRITE_PIPE_LEFT
         elif self.block_type == "PIPE_RIGHT":
             sprite = Constants.SPRITE_PIPE_RIGHT
+        elif self.block_type == "FLOOR":
+            sprite = Constants.SPRITE_FLOOR
         return sprite
-
-
-
