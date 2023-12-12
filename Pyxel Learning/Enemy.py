6c7148d7d79e27eda4b3955a9254526eba5a372d
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-12 22:19:46 +0100

    Update Enemy.py

--- Pyxel Learning/Enemy.py
+++ Pyxel Learning/Enemy.py
@@ -1,49 +1,63 @@
 import random
 import pyxel
-from Constants import *
+import Constants
+
 
 class Enemy:
-    """Class that represents an enemy and is being inherited as the base class for all the enemies subclasses.
-
-    Attributes:
-        x (float): X coordinate of the enemy
-        y (float): Y coordinate of the enemy
-        alive (bool): Is the enemy alive or not
-        width (int)(readonly): Width of the enemy
-        height (int)(readonly): Height of the enemy
-        speed (int)(readonly): Speed of the enemy
-
-    Methods:
-        update(): Updates the enemy position and removes the shots that go off the screen
-    """
-
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: int, y: int):
+        self.paused = False
         self.x = x
         self.y = y
-        self.width = 10
-        self.height = 10
+        self.width = 16
+        self.height = 16
         self.speed = 1
         self.alive = True
+        self.direction = random.choice([-1, 1])
+        self.max_dx = 2
+        self.dy = 0
+        self.gravity = 0.5
+        self.pause_duration = 100
+        self.pause_end_frame = 0
+        self.color = 'NORMAL'
 
     def update(self):
-        if self.alive:
-            self.x += self.speed
+        if not self.paused:
+            self.dy += self.gravity
+            self.y += self.dy
+            if self.x - self.width < -30 or self.x > pyxel.width + 30 or self.y - self.height < -30 or self.y > pyxel.height + 30:
+                self.alive = False
+            elif self.direction == -1:
+                self.x -= self.speed
+            elif self.direction == 1:
+                self.x += self.speed
+
+            if self.y > Constants.FLOOR:
+                self.y = Constants.FLOOR
+                self.dy = 0
+
+            if self.x < Constants.PIPE_X_POSITION_LEFT and self.x < 0 and self.y > 150:
+                self.x = 2
+                self.y = 34
+                self.direction = 1
+
+            if self.x < Constants.PIPE_X_POSITION_LEFT and self.x < 0 and self.y <= 150:
+                self.x = 254  # Reappear on the right side of the screen
+                # Optionally, you can change other properties here, such as direction
+
+            # Check if the enemy went through the right pipe and crossed the screen
+            if self.x > Constants.PIPE_X_POSITION_RIGHT and self.x > 256 and self.y >= 150:
+                self.x = 254
+                self.y = 34
+                self.direction = -1
+
+            if self.x > Constants.PIPE_X_POSITION_RIGHT and self.x > 256 and self.y <= 150:
+                self.x = 2
 
 
-        # If the enemy goes off of the screen by more the 30 pixels (dead)
-        if self.x - self.width < -30 or self.x > pyxel.width + 30 or self.y - self.height < -30 or self.y > pyxel.height + 30:
-            self.health = 0
+        else:
 
+            if self.pause_end_frame == 0:
+                self.pause_end_frame = pyxel.frame_count + self.pause_duration
 
-
-    def draw(self):
-        if self.alive:
-            pyxel.rect(self.x, self.y, self.x + self.width, self.y + self.height, pyxel.COLOR_RED)
-
-
-
-# Example usage:
-if __name__ == "__main__":
-    pyxel.init(160, 120, title="Mario Enemy Example")
-    enemy = Enemy(20, 20)  # Create an enemy instance
-    pyxel.run(enemy.update, enemy.draw)
+            if pyxel.frame_count >= self.pause_end_frame:
+                self.paused = False
