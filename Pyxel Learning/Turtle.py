42801f920d8c7bcd17d0aa64fe18ad5caff5d355
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-12 22:21:03 +0100

    Update Turtle.py

--- Pyxel Learning/Turtle.py
+++ Pyxel Learning/Turtle.py
@@ -2,24 +2,29 @@ from Enemy import Enemy
 import random
 import pyxel
 
+
 class Turtle(Enemy):
     def __init__(self, x: int, y: int):
         super().__init__(x, y)
         self.shell_color = (0, 255, 0)
-        self.direction_speed = random.choice([-1,1])
+        self.direction_speed = random.choice([-1, 1])
         self.lives = 2
+        self.paused = False
 
     def move(self):
-        if self.x == 0:
-            self.direction_speed = 1  # Move right if spawned at x = 0
-        elif self.x == pyxel.width:
-            self.direction_speed = -1
+        if not self.paused:
+            if self.x == 0:
+                self.direction_speed = 1  # Move right if spawned at x = 0
+            elif self.x == pyxel.width:
+                self.direction_speed = -1
 
     def draw(self):
-        """Draws the enemy"""
-        if self.direction_speed == -1:
-            pyxel.blt(self.x, self.y, 0, 48, 0, 16, 16, 0)
+        if self.paused and (self.direction_speed == 1 or self.direction_speed == -1):
+            pyxel.blt(self.x, self.y, 0, 112, 184, 16, 16, 0)
+        elif self.direction_speed == -1:
+            pyxel.blt(self.x, self.y, 0, 0, 184, 16, 16, 0)
         elif self.direction_speed == 1:  # Change to 1 for the other direction
-            pyxel.blt(self.x, self.y, 0, 48, 0, 16, 16, 0)
+            pyxel.blt(self.x, self.y, 0, 64, 184, 16, 16, 0)
+
         else:
             raise Exception("The direction of the enemy is not supported")
