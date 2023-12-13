759c865f5a2749915f3a8c91a73f0732eefcb44d
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-13 19:20:31 +0100

    Update Turtle.py

--- Pyxel Learning/Turtle.py
+++ Pyxel Learning/Turtle.py
@@ -1,21 +1,17 @@
+from Enemy import Enemy
 import random
 import pyxel
-import Constants
-from Enemy import Enemy
 
 
-class Flies(Enemy):
+class Turtle(Enemy):
     def __init__(self, x: int, y: int):
         super().__init__(x, y)
         self.shell_color = (0, 255, 0)
         self.direction_speed = random.choice([-1, 1])
         self.lives = 1
         self.paused = False
-        self.jump_strength = -2
-        self.jumping = False
-        self.jumper = True
-        self.gravity = 0.1
-        self.speed = 0.5
+        self.jumper = False
+        self.color = 'NORMAL'
 
     def move(self):
         if not self.paused:
@@ -24,11 +20,16 @@ class Flies(Enemy):
             elif self.x == pyxel.width:
                 self.direction_speed = -1
 
-
     def draw(self):
-        if self.paused and (self.direction_speed == 1 or self.direction_speed == -1):
-            pyxel.blt(self.x, self.y, 1, 29, 178, 15 * self.direction_speed, 16, 0)
-        elif not self.paused:
-            pyxel.blt(self.x, self.y, 1, 29, 154, 15 * self.direction_speed, 16, 0)
+        if self.color == 'NORMAL':
+            if self.paused and (self.direction_speed == 1 or self.direction_speed == -1):
+                pyxel.blt(self.x, self.y, 0, 112, 184, 16, 16, 0)
+            elif not self.paused:
+                pyxel.blt(self.x, self.y, 0, 0, 184, 16 * self.direction, 16, 0)
+        elif self.color == "CHANGED":
+            if self.paused and (self.direction_speed == 1 or self.direction_speed == -1):
+                pyxel.blt(self.x, self.y, 0, 200, 184, 16 * self.direction_speed, 16, 0)
+            elif not self.paused:
+                pyxel.blt(self.x, self.y, 0, 184, 184, 16 * self.direction, 16, 0)
         else:
             raise Exception("The direction of the enemy is not supported")
