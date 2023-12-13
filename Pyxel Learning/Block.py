171b3a905ec855973b40a33f5447ba09f97fa764
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-13 18:48:40 +0100

    Update Block.py

--- Pyxel Learning/Block.py
+++ Pyxel Learning/Block.py
@@ -15,33 +15,34 @@ class Block:
         self.width = 16
         self.height = 8
         self.block_hit = False
-        self.animation_frames = 0
+        self.timer = 0
+        self.used_pow = False
+        self.pow_counter = 0
 
 
-    def update(self):
-        if self.block_hit:
-            if self.animation_frames < 3:
+    def pow(self):
+        if self.timer == 0:
+            self.timer = pyxel.frame_count + 120
 
-                self.block_type = f'HIT{self.animation_frames + 1}'
-                print(self.block_type)
-
-                self.animation_frames += 1
-            else:
-
-                self.block_hit = False
-                self.block_type = 'BLOCK1'
-                self.animation_frames = 0
+        if not pyxel.frame_count >= self.timer:
+            self.used_pow = False
+            self.timer = 0
+            self.pow_counter += 1
 
     @property
     def sprite(self) -> tuple:
         global sprite
         if self.block_type == "BLOCK1":
             sprite = Constants.SPRITE_BLOCK_1
-        elif self.block_type == 'HIT1':
-            sprite = Constants.HIT_BLOCK_1
         elif self.block_type == "BLOCK2":
             sprite = Constants.SPRITE_BLOCK_2
-        elif self.block_type == "POW":
+        elif self.block_type == "BLOCK3":
+            sprite = Constants.SPRITE_BLOCK_3
+        elif self.block_type == "BLOCK4":
+            sprite = Constants.SPRITE_BLOCK_4
+        elif self.block_type == "POW" and not self.used_pow:
+            sprite = Constants.SPRITE_POW
+        elif self.block_type == "POW" and self.used_pow:
             sprite = Constants.SPRITE_POW
         elif self.block_type == "PIPE_LEFT":
             sprite = Constants.SPRITE_PIPE_LEFT
@@ -49,4 +50,24 @@ class Block:
             sprite = Constants.SPRITE_PIPE_RIGHT
         elif self.block_type == "FLOOR":
             sprite = Constants.SPRITE_FLOOR
+        elif self.block_type == 'PIPE_RIGHT_UPPER':
+            sprite = Constants.SPRITE_PIPE_RIGHT_UPPER
+        elif self.block_type == 'PIPE_RIGHT_LOWER':
+            sprite = Constants.SPRITE_PIPE_RIGHT_LOWER
+        elif self.block_type == 'PIPE_RIGHT_UPPER_LEFT':
+            sprite = Constants.SPRITE_PIPE_RIGHT_UPPER_LEFT
+        elif self.block_type == 'PIPE_RIGHT_LOWER_LEFT':
+            sprite = Constants.SPRITE_PIPE_RIGHT_LOWER_LEFT
+        elif self.block_type == 'PIPE_RIGHT_UPPER_RIGHT':
+            sprite = Constants.SPRITE_PIPE_RIGHT_UPPER
+        elif self.block_type == 'PIPE_RIGHT_LOWER_RIGHT':
+            sprite = Constants.SPRITE_PIPE_RIGHT_LOWER
+        elif self.block_type == 'PIPE_LEFT_UPPER_LEFT':
+            sprite = Constants.SPRITE_PIPE_LEFT_UPPER_LEFT
+        elif self.block_type == 'PIPE_LEFT_LOWER_LEFT':
+            sprite = Constants.SPRITE_PIPE_LEFT_LOWER_LEFT
+        elif self.block_type == 'PIPE_LEFT_UPPER_RIGHT':
+            sprite = Constants.SPRITE_PIPE_LEFT_UPPER_RIGHT
+        elif self.block_type == 'PIPE_LEFT_LOWER_RIGHT':
+            sprite = Constants.SPRITE_PIPE_LEFT_LOWER_RIGHT
         return sprite
