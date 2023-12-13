dd9e2407066a0b9b0faf034efb7ba032fd54c321
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-13 18:43:57 +0100

    Update Enemy.py

--- Pyxel Learning/Enemy.py
+++ Pyxel Learning/Enemy.py
@@ -5,11 +5,10 @@ import Constants
 
 class Enemy:
     def __init__(self, x: int, y: int):
+        self.lives = 2
         self.paused = False
         self.x = x
         self.y = y
-        self.width = 16
-        self.height = 16
         self.speed = 1
         self.alive = True
         self.direction = random.choice([-1, 1])
@@ -19,6 +18,22 @@ class Enemy:
         self.pause_duration = 100
         self.pause_end_frame = 0
         self.color = 'NORMAL'
+        self.jumping = False
+        self.jump_strength = -5
+        self.jumper = True
+        self.jump_delay = 30
+        self.timer = 0
+        self.collision = 0
+
+
+    @property
+    def width(self):
+        return 16
+
+    @property
+    def height(self):
+        return 14
+
 
     def update(self):
         if not self.paused:
@@ -53,11 +68,36 @@ class Enemy:
             if self.x > Constants.PIPE_X_POSITION_RIGHT and self.x > 256 and self.y <= 150:
                 self.x = 2
 
+            if self.jumper and not self.jumping:
+                if self.timer == 0:
+                    self.timer = pyxel.frame_count + self.jump_delay
+
+                if pyxel.frame_count >= self.timer:
+                    self.jumping = True
+                    self.jump()
+                    self.y += self.dy
+
 
         else:
 
             if self.pause_end_frame == 0:
                 self.pause_end_frame = pyxel.frame_count + self.pause_duration
+                self.lives -= 1
 
             if pyxel.frame_count >= self.pause_end_frame:
                 self.paused = False
+                self.enemy_change_color()
+                self.pause_end_frame = 0
+
+    def jump(self):
+        if self.jumping:
+            self.dy = self.jump_strength
+
+
+        if self.y >= Constants.FLOOR:
+            self.jumping = False
+            self.y = Constants.FLOOR
+            self.dy = 0
+
+    def enemy_change_color(self):
+        self.color = 'CHANGED'
