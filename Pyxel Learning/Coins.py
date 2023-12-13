35c9febcbd156c513e0a172ae603d3ee098c97af
CollinMartin12 <153111660+CollinMartin12@users.noreply.github.com>
2023-12-13 18:45:54 +0100

    Update Coins.py

--- Pyxel Learning/Coins.py
+++ Pyxel Learning/Coins.py
@@ -1,42 +1,26 @@
 import pyxel
 import random
-import Constants
+from Enemy import Enemy
 
 
-class Coins:
+class Coins(Enemy):
 
-    def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
-        self.speed = 4
-        self.direction = random.choice([-1, 1])
-        self.dy = 0
-        self.gravity = 0.5
-        self.actionable = True
 
-    @property
-    def width(self):
-        return 4
+    def __init__(self, x: int, y: int):
+        super().__init__(x, y)
+        self.collected = False
+        self.direction_speed = random.choice([-2, 2])
+        self.jumper = False
 
-    @property
-    def height(self):
-        return 4
 
-    def update(self):
-        if self.actionable:
-            self.dy += self.gravity
-            self.y += self.dy
-            if self.x - self.width < -30 or self.x > pyxel.width + 30 or self.y - self.height < -30 or self.y > pyxel.height + 30:
-                self.actionable = False
-            elif self.direction == -1:
-                self.x -= self.speed
-            elif self.direction == 1:
-                self.x += self.speed
 
-            if self.y > Constants.FLOOR:
-                self.y = Constants.FLOOR
-                self.dy = 0
+    def move(self):
+        if not self.collected:
+            if self.x == 0:
+                self.direction_speed = 1  # Move right if spawned at x = 0
+            elif self.x == pyxel.width:
+                self.direction_speed = -1
 
     def draw(self):
-        """Draws the shot"""
+        '''draws the coin'''
         pyxel.blt(self.x, self.y, 0, 96, 154, self.width, self.height, 0)
